f1d37af523d006a89175d3cb2e867b42263e956d
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-04-29 13:17:07 +0100

    rename dir

--- bgcval/p2p/__init__.py
+++
@@ -1,5 +0,0 @@
-#from makeTargets import makeTargets
-#from p2pPlots import makePlots
-#from matchDataAndModel import matchDataAndModel
-#from makePatternStatsPlots import makePatternStatsPlots
-#from  testsuite_p2p import  testsuite_p2p
