760965aede0f4e98a572b26d289584fe087bca58
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-08-18 17:12:18 +0100

    add test loading keys

--- tests/integration/test_analysis_timeseries.py
+++ tests/integration/test_analysis_timeseries.py
@@ -1,9 +1,13 @@
 """Test funcs in analysis_timeseries."""
+import bgcval2
 import os
 import pytest
 import yaml
 
-from bgcval2.analysis_timeseries import load_function
+from bgcval2.analysis_timeseries import (
+    load_function,
+    load_key_file
+)
 
 def test_load_function(tmp_path):
     """Test load_function()."""
@@ -34,7 +38,6 @@ def test_load_function_kwargs(tmp_path):
 # test globalVolumeMean
 def test_load_function_internal(tmp_path):
     """Test load_function()."""
-    import bgcval2
     package_path = os.path.dirname(bgcval2.__file__)
     func_file = os.path.join(package_path, 'functions', 'globalVolMean.py')
     convert = {
@@ -45,3 +48,38 @@ def test_load_function_internal(tmp_path):
         res = expected_func("gt40.nc", keys={})
     assert "Needs an `areafile` in kwargs" in str(exc)
     assert expected_kwargs == {"cow": "moo"}
+
+
+def test_load_key_file(tmp_path):
+    """Test load_key_file() func."""
+    package_path = os.path.dirname(os.path.dirname(bgcval2.__file__))
+    class Object(object):
+        pass
+    paths = Object()
+    paths.bgcval2_repo = package_path
+    paths.orcaGridfn = str(tmp_path / "dummy_orca")
+    with open(paths.orcaGridfn, "w") as fil:
+        fil.write("free Willy!")
+    paths.ModelFolder_pref = "moo"
+    paths.ObsFolder = "ox"
+    key = "temperature"
+    jobID = "cow"
+    runtime_output_dict = load_key_file(key, paths, jobID)
+    expected_output_dict = dict()
+    expected_output_dict['modeldetails'] = dict()
+    expected_output_dict['name'] = 'Temperature'
+    assert expected_output_dict['name'] == runtime_output_dict['name']
+    expected_output_dict['units'] = 'degrees C'
+    assert expected_output_dict['units'] == runtime_output_dict['units']
+    expected_output_dict['dimensions'] = 3
+    assert expected_output_dict['dimensions'] == runtime_output_dict['dimensions']
+    expected_output_dict['layers'] = ['Surface']
+    assert expected_output_dict['layers'] == runtime_output_dict['layers']
+    expected_output_dict['gridFile'] = paths.orcaGridfn
+    assert expected_output_dict['gridFile'] == runtime_output_dict['gridFile']
+    expected_output_dict['modeldetails']['name'] = 'Temperature'
+    assert expected_output_dict['modeldetails']['name'] == \
+        runtime_output_dict['modeldetails']['name']
+    expected_output_dict['modeldetails']['vars'] = ['thetao_con']
+    assert expected_output_dict['modeldetails']['vars'] == \
+        runtime_output_dict['modeldetails']['vars']
