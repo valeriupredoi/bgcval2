56a956c8948abb57c9999570560a0fbb5c8c0b1a
Lee de Mora <ledm@pml.ac.uk>
2022-08-18 08:25:32 +0100

    Update bgcval2/functions/standard_functions.py
    Co-authored-by: Valeriu Predoi <valeriu.predoi@gmail.com>

--- bgcval2/functions/standard_functions.py
+++ bgcval2/functions/standard_functions.py
@@ -155,7 +155,7 @@ def multiplyBy(nc,keys, **kwargs):
     Loads keys[0] from the netcdf, but multiplies by the field in kwargs , "factor".
     """
     if 'factor' not in kwargs:
-        raise AssertionError("std_functions:\tmultiplyBy:\t Did not get key word argument, 'factor'.")
+        raise KeyError(f"std_functions:\tmultiplyBy:\t Did not get key word argument, 'factor' in kwargs {str(kwargs)}")
     return nc.variables[keys[0]][:]* float(kwargs['factor'])
 
     
