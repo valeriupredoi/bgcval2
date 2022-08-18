ef1017e0228ce1f8a1b0ffbf903469868888c139
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-08-18 12:24:11 +0100

    added extra test

--- tests/integration/test_analysis_timeseries.py
+++ tests/integration/test_analysis_timeseries.py
@@ -15,3 +15,17 @@ def test_load_function(tmp_path):
     res = expected_func()
     assert res == 22
     assert expected_kwargs == {}
+
+
+def test_load_function_kwargs(tmp_path):
+    """Test load_function()."""
+    dummy_func_file = tmp_path / 'myfuncfile2.py'
+    with open(dummy_func_file, "w") as fil:
+        fil.write("import bgcval2\n\ndef myfunc():\n    return 22\n")
+    convert = {
+        "path": str(dummy_func_file), "function": "myfunc",
+        "cow": "moo"}
+    expected_func, expected_kwargs = load_function(convert)
+    res = expected_func()
+    assert res == 22
+    assert expected_kwargs == {"cow": "moo"}
