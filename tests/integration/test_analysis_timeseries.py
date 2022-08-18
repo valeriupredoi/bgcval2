82e1855b0dbef7be56a95a4454e61e2f9d278c0b
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-08-18 13:17:09 +0100

    add test for internal function loading

--- tests/integration/test_analysis_timeseries.py
+++ tests/integration/test_analysis_timeseries.py
@@ -29,3 +29,19 @@ def test_load_function_kwargs(tmp_path):
     res = expected_func()
     assert res == 22
     assert expected_kwargs == {"cow": "moo"}
+
+
+# test globalVolumeMean
+def test_load_function_internal(tmp_path):
+    """Test load_function()."""
+    import bgcval2
+    package_path = os.path.dirname(bgcval2.__file__)
+    func_file = os.path.join(package_path, 'functions', 'globalVolMean.py')
+    convert = {
+        "path": str(func_file), "function": "globalVolumeMean",
+        "cow": "moo"}
+    expected_func, expected_kwargs = load_function(convert)
+    with pytest.raises(KeyError) as exc:
+        res = expected_func("gt40.nc", keys={})
+    assert "Needs an `areafile` in kwargs" in str(exc)
+    assert expected_kwargs == {"cow": "moo"}
