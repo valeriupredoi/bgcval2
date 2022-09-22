2d6d24fc3c8b3e9e65e59544e7f0e10c29e9a64b
Lee de Mora <ledm@pml.ac.uk>
2022-09-22 17:09:13 +0100

    added cron line to comments.

--- scripts/shared_mass_cron.sh
+++ scripts/shared_mass_cron.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# single script to automatically download bgcval2 users data.
+# 
+# cron tab line:
+# 00 19 * * * /gws/nopw/j04/esmeval/bgcval2/shared_mass_cron.sh >  /gws/nopw/j04/esmeval/bgcval2/shared_mass_cron.out 2>&1 # bgcval2 automatic download 
+
+
 #self-destruct:
 {
     sleep 1380m  # Kill script after 23 hours.
