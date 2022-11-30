c332bf1a071f4b75bc4772f433835f6e9f58a022
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-11-30 12:34:45 +0000

    add mass scripts dir

---
+++ mass_scripts/u-cp647debug.sh
@@ -0,0 +1,7 @@
+# Run this script on mass-cli1.jasmin.ac.uk
+# from login1.jasmin.ac.uk, ssh to the mass machine:
+#     ssh -X  mass-cli
+# run script with:
+# source /home/valeriu/bgcval2/mass_scripts/u-cp647debug.sh
+# moo passwd -r # if mass password is expired
+moo get --fill-gaps moose:/crum/u-cp647debug/ony.nc.file/*.nc /home/valeriu/bgcval2/local_test/BGC_data/u-cp647debug/
