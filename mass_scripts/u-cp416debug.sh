c30785e212f04576e3ed5861c145c6c4b528864e
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-12-01 16:38:23 +0000

    rm unwanted dir

--- mass_scripts/u-cp416debug.sh
+++
@@ -1,7 +0,0 @@
-# Run this script on mass-cli1.jasmin.ac.uk
-# from login1.jasmin.ac.uk, ssh to the mass machine:
-#     ssh -X  mass-cli
-# run script with:
-# source /home/valeriu/bgcval2/mass_scripts/u-cp416debug.sh
-# moo passwd -r # if mass password is expired
-moo get --fill-gaps moose:/crum/u-cp416debug/ony.nc.file/*.nc /home/valeriu/bgcval2/local_test/BGC_data/u-cp416debug/
