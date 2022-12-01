b823fab84682f8400f49ea2f2da26cd190f3d36b
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-12-01 14:34:42 +0000

    rename test script

---
+++ tests/integration/test_analysis_compare_end-end.py
@@ -0,0 +1,47 @@
+"""
+Run end-to-end test/dummy-data analyses.
+
+Dummy data is produced with produce_dummy_data.py (needs iris),
+but since BGCVal2 needs a LOT of data, we don't store it but
+produce it on the fly. Some dummy data still needs some fixing
+to fully work with various components of BGCVal2, hence some tests
+here catch errors rather than run full end-to-end analyses.
+
+"""
+import contextlib
+import functools
+import pytest
+import sys
+
+import bgcval2
+
+from bgcval2.analysis_compare import main
+from unittest.mock import patch
+
+
+def wrapper(f):
+    @functools.wraps(f)
+    def empty(*args, **kwargs):
+        if kwargs:
+            raise ValueError(f'Parameters not supported: {kwargs}')
+        return True
+
+    return empty
+
+
+@contextlib.contextmanager
+def arguments(*args):
+    backup = sys.argv
+    sys.argv = list(args)
+    yield
+    sys.argv = backup
+
+
+@patch('bgcval2.analysis_compare.main', new=wrapper(bgcval2.analysis_compare.main))
+def test_run_analysis_compare():
+    """Patch and run the whole thing."""
+    with pytest.raises(ValueError) as exc:
+        with arguments('analysis_compare', '-y', 'input_yml/debug.yml'):
+            main()
+    expected_exc = "operands could not be broadcast together with shapes (4,299,299) (3,) (4,299,299)"
+    assert expected_exc in str(exc.value)
