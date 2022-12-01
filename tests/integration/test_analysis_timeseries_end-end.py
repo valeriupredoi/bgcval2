e90614059851f0c095b44330a870a273c798263a
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-12-01 14:34:16 +0000

    run ee timeseries

---
+++ tests/integration/test_analysis_timeseries_end-end.py
@@ -0,0 +1,45 @@
+"""
+Run end-to-end test/dummy-data analyses.
+
+Dummy data is produced with produce_dummy_data.py (needs iris),
+but since BGCVal2 needs a LOT of data, we don't store it but
+produce it on the fly. Some dummy data still needs some fixing
+to fully work with various components of BGCVal2, hence some tests
+here catch errors rather than run full end-to-end analyses.
+
+"""
+import contextlib
+import functools
+import pytest
+import sys
+
+import bgcval2
+
+from bgcval2.analysis_timeseries import main
+from unittest.mock import patch
+
+
+def wrapper(f):
+    @functools.wraps(f)
+    def empty(*args, **kwargs):
+        if kwargs:
+            raise ValueError(f'Parameters not supported: {kwargs}')
+        return True
+
+    return empty
+
+
+@contextlib.contextmanager
+def arguments(*args):
+    backup = sys.argv
+    sys.argv = list(args)
+    yield
+    sys.argv = backup
+
+
+@patch('bgcval2.analysis_timeseries.main', new=wrapper(bgcval2.analysis_timeseries.main))
+def test_run_analysis_timeseries_debug():
+    """Patch and run the whole thing."""
+    with arguments('analysis_timeseries', '--jobID', 'u-cp416debug',
+                   '--keys', 'debug'):
+        main()
