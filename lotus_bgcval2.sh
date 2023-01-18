408afd28d94e4fad8eb4a4d49d3c3f45fd1e813f
Lee de Mora <ledm@pml.ac.uk>
2023-01-18 15:31:11 +0000

    replaced bsub with sbatch

--- lotus_bgcval2.sh
+++ lotus_bgcval2.sh
@@ -1,13 +1,10 @@
+
 #!/bin/bash
-#BSUB -q short-serial
-#BSUB -n 1
-#BSUB -W 24:00
-#BSUB -o "logs/bgcval2%J.out"
-#BSUB -e "logs/bgcval2%J.err"
-#BSUB -J bgcval2
-#BSUB -u EMAIL@email.com    
-#BSUB -B
-#BSUB -N
+#SBATCH --job-name=bgcval2
+#SBATCH --partition=short-serial
+#SBATCH --time 24:00
+#SBATCH -o log_bgcval2_%J.out
+#SBATCH -e log_bgcval3_%J.err
 
 
 ######################
@@ -15,61 +12,67 @@
 # rysnc -av lotus_bgcval2.sh lotus_bgcval2_$USER.sh
 #
 # Then edit your new copy (lotus_bgcval2_$USER.sh):
-#  1. Change your email above (#BSUB -u email@email.com)
 #
-#  2. Check the name of your conda environment: CONDA_ENV 
+#  1. Check the name of your conda environment: CONDA_ENV
 #     "bgcval2" is the default, but may have a different one.
 #
-#  3. Change the path to your bgcval2 directory: BGCVAL2_PATH
+#  2. Check the path to your bgcval2 directory: BGCVAL2_PATH
 #
-#  4. Change the analyses to run: BGCVAL2_SUITE
-#     You can add several suites here if needed. 
+#  3. Change the analyses to run: BGCVAL2_SUITE
+#     You can add several suites here if needed.
 #
-#  5. Submit this script with with:
-#     bsub <  lotus_bgcval2_$USER.sh
+#  4. Submit this script with with:
+#     sbatch lotus_bgcval2_$USER.sh
 #
-#  6. Monitor progress with:
-#     bjobs | grep $USER
+#  5. Monitor progress with:
+#     squeue | grep $USER
+#
 #########################
 
 
+
+#########################
 # Edit these bits:
 
 # Change this to your bgcval2 conda environment name
 CONDA_ENV=bgcval2
+
 # Change this to your bgcval2 directory - or wheever you wish to run.
-BGCVAL2_PATH=/home/users/$USER/bgcval2
+BGCVAL2_PATH=/home/users/$USER//bgcval2
+
 # Add one or more input_yml files here
-BGCVAL2_SUITE=input_yml/my_bgcval2_suite.yml
-#BGCVAL2_SUITE2=input_yml/my_bgcval2_suite2.yml
+BGCVAL2_SUITE2=input_yml/my_bgcval2_suite2.yml
 
 
 ##########################
-# script
+# Source global definitions
+if [ -f ~/.bashrc ]; then
+    echo 'source ~/.bashrc'
+    source ~/.bashrc
+fi
 
-# load the conda program:
-module load jaspy
-
+##########################
 # load your conda env:
-conda activate $CONDA_ENV
+#conda activate bgcval2
+echo conda activate $CONDA_ENV
+conda activate ${CONDA_ENV}
 
+##########################
 # Change directory to your bgcval2 directory:
+echo cd $BGCVAL2_PATH
 cd $BGCVAL2_PATH
-
-
+pwd
 
 #########################
-# Add one or more input_yml files here 
+# Add one or more input_yml files here
+echo analysis_compare -y $BGCVAL2_SUITE
 analysis_compare -y $BGCVAL2_SUITE
-#analysis_compare -y $BGCVAL2_SUITE2
-
-
 
 #########################
 # Rsync report to web facing directory:
 # (shouldn't need to change this!)
-Outpath=/gws/nopw/j04/esmeval/public/CompareReports/bgcval2/$USER
-mkdir -p $Outpath
-rsync -av CompareReports2/* $Outpath/.
+OUTPATH=/gws/nopw/j04/esmeval/public/CompareReports/bgcval2/$USER
+mkdir -p $OUTPATH
+rsync -av CompareReports2/* $OUTPATH/.
 
 
