Added correct units to intpp calculation.

bgcval2/functions/TotalIntPP.py
@@ -49,6 +49,7 @@ def loadDataMask(gridfn):
     loadedArea = True
     return model_area
 
+
 def TotalIntPP(nc, keys, **kwargs):
     """
     This function calculated the total primary production for the MEDUSA model in the eORCA grid.
@@ -61,14 +62,15 @@ def TotalIntPP(nc, keys, **kwargs):
     #    mmolN/m2/d        [mg C /m2/d]   [mgC/m2/yr] [gC/m2/yr]     Gt/m2/yr # MEDUSA UNITS
     factor = 1.     * 6.625 * 12.011 * 365.       / 1000.   /     1E15
     arr = (nc.variables[keys[0]][:]+ nc.variables[keys[1]][:]).squeeze()*factor
-    if arr.ndim ==3:
+    if arr.ndim == 3:
         for i in np.arange(arr.shape[0]):
             arr[i] = arr[i]*model_area
-    elif arr.ndim ==2:
+    elif arr.ndim == 2:
         arr = arr*model_area
     else:
         raise ValueError(f'TotalIntPP: arr shape not recognised, should be either 2 or 3, got value {arr.shape} in {nc.filename}')
     return arr.sum()
+
 
 def MA_TotalIntPP(nc, keys, **kwargs):
     """
@@ -77,16 +79,22 @@ def MA_TotalIntPP(nc, keys, **kwargs):
     areafile = get_kwarg_file(kwargs, 'areafile')
     if not loadedArea:
         model_area = loadDataMask(areafile)
-    # [mgC/m2/yr] [gC/m2/yr]     Gt/m2/yr
-    # mg C/m^3/d (supposedly - but possibly /m2 ?) 
+    # mg C/m^3/d (supposedly - but possibly /m2 ?)
     factor = 365.25       / 1000.   /     1E15
-    arr = nc.variables[keys[0]][:].squeeze()*factor
-    if arr.ndim ==3:
-        for i in np.arange(arr.shape[0]):
-            arr[i] = arr[i]*model_area
-    elif arr.ndim ==2:
-        arr = arr*model_area
-    else:
-        raise ValueError(f'MA_TotalIntPP: arr shape not recognised, should be either 2 or 3, got value {arr.shape} in {nc.filename}')
+    area = nc.variables['area_grid_T'][:]
+    thick = nc.variables['e3w'][:]
+    arr = thick * nc.variables[keys[0]][:]*factor
+    arr = arr.sum(axis=1)
+    arr = arr * area[None, ...]
+    #print('MA_TotalIntPP', keys[0], arr.sum())
     return arr.sum()
+
+#    if arr.ndim == 3:
+#        for i in np.arange(arr.shape[0]):
+#            arr[i] = arr[i]*model_area
+#    elif arr.ndim == 2:
+#        arr = arr*model_area
+#    else:
+#        raise ValueError(f'MA_TotalIntPP: arr shape not recognised, should be either 2 or 3, got value {arr.shape} in {nc.filename}')
+#    return arr.sum()
 