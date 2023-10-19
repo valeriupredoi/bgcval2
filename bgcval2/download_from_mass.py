55666bac24cecb605d20d4951cd179a45f62d041
Lee de Mora <ledm@pml.ac.uk>
2023-10-19 14:50:06 +0100

    Added permission check before editing files/copying

--- bgcval2/download_from_mass.py
+++ bgcval2/download_from_mass.py
@@ -37,6 +37,7 @@ import subprocess
 from socket import gethostname
 import shutil
 import os
+from pwd import getpwuid
 import stat
 from glob import glob
 from re import findall
@@ -409,7 +410,16 @@ def download_from_mass(
     outputFold = folder([paths.ModelFolder_pref,  jobID,] )
     # make this folder group writeable. 
     st = os.stat(outputFold)
-    os.chmod(outputFold, st.st_mode | stat.S_IWGRP)
+    
+    # Case one:
+    # I just created this folder and I own it: 
+    i_can_write_this = os.access(outputFold, os.W_OK)
+    if i_can_write_this:
+        os.chmod(outputFold, st.st_mode | stat.S_IWGRP)
+    else:
+        uname = getpwuid(os.stat(outputFold).st_uid).pw_name
+        print('WARNING: someone else (', uname, ') owns this directory, so you may not be able to download files.')
+        print('WARNING: Ask them politely to run "chmod g+w ',outputFold,'"')
 
     deleteBadLinksAndZeroSize(outputFold, jobID)
 
@@ -483,8 +493,19 @@ def download_from_mass(
     if auto_download:
         shared_file_path = os.path.join(paths.shared_mass_scripts, os.path.basename(download_script_path))
         print('writing file in shared path', shared_file_path)
-        shutil.copy(download_script_path, shared_file_path)
 
+        # Case one:
+        # I just created this folder and I own it:
+        i_can_write_this = os.access(shared_file_path, os.W_OK)
+        if i_can_write_this:
+            os.chmod(download_script_path, st.st_mode | stat.S_IWGRP)
+            shutil.copy(download_script_path, shared_file_path)
+            os.chmod(shared_file_path, st.st_mode | stat.S_IWGRP)
+        else:
+            uname = getpwuid(os.stat(shared_file_path).st_uid).pw_name
+            print('WARNING: someone else (', shared_file_path, ') owns this directory, so you may not be able to download files.')
+            print('WARNING: Ask them politely to run "chmod g+w ', shared_file_path,'"')
+        
     fixFilePaths(outputFold, jobID, debug=False,)
     deleteBadLinksAndZeroSize(outputFold, jobID, debug=False,)
 
@@ -598,12 +619,17 @@ def deleteBadLinksAndZeroSize(outputFold, jobID, debug=True):
     bashCommand1 = "find " + outputFold + "/. -size 0 -print -delete"
     bashCommand2 = "find -L " + outputFold + "/. -type l -delete  -print"
 
-    if debug: print("deleteBadLinksAndZeroSize:\t", bashCommand1)
+    bashCommand1 = bashCommand1.replace('//', '/')
+    bashCommand2 = bashCommand2.replace('//', '/')
+
+    #f debug: 
+    print("deleteBadLinksAndZeroSize:\t", bashCommand1)
 
     process1 = subprocess.Popen(bashCommand1.split(), stdout=subprocess.PIPE)
     output1 = process1.communicate()[0]
 
-    if debug: print("deleteBadLinksAndZeroSize:\t", bashCommand2)
+    #f debug: 
+    print("deleteBadLinksAndZeroSize:\t", bashCommand2)
 
     process2 = subprocess.Popen(bashCommand2.split(), stdout=subprocess.PIPE)
     output2 = process2.communicate()[0]
