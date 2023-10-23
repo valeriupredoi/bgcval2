098ad28c3dd47f25494e0b69b3064b819a6ceef0
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-10-23 16:09:08 +0100

    fix test

--- tests/unit/test_download_from_mass_permissions.py
+++ tests/unit/test_download_from_mass_permissions.py
@@ -6,14 +6,16 @@ from bgcval2.download_from_mass import download_from_mass as dfm
 
 
 def test_allowed_user():
+    user_home = os.path.expanduser('~')
+    output_folder = "bgcval2/local_test/BGC_data/u-xxx/"
+    run_dir = os.path.join(user_home, output_folder)
+    if not os.path.exists(run_dir):
+        os.makedirs(run_dir)
     dfm(jobID="u-xxx",
         doMoo=False,
         auto_download=False,
         config_user="defaults"
     )
-    user_home = os.path.expanduser('~')
-    output_folder = "bgcval2/local_test/BGC_data/u-xxx/"
-    run_dir = os.path.join(user_home, output_folder)
     assert os.stat(run_dir).st_mode == 16893
 
 
