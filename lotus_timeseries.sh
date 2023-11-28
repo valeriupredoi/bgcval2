4f82bfd162c6d0ffa12edcc9b4012cca96489c40
Lee de Mora <ledm@pml.ac.uk>
2023-11-28 10:35:11 +0000

    changed comments

--- lotus_timeseries.sh
+++ lotus_timeseries.sh
@@ -4,14 +4,19 @@
 #SBATCH -o logs/log_bgcval2_ts_%J.out
 #SBATCH -e logs/log_bgcval3_ts_%J.err
 
+# Note that the 
 # slurm job name should be set in the command line with -J option to the jobID
+# and the output and error filers are also set at the command line.
+#
 
 ######################
 # This script runs a single time series job.
 #
 # Runs:
 #    sbatch -J jobID lotus_timeseries.sh jobID suite1 suite2 etc...
-#
+# Note that that batch_timeseries command also
+# adds specific out and job scripts,
+# and outputs them to ./logs directory..  
 #########################
 
 
@@ -19,11 +24,6 @@
 # Change this to your bgcval2 conda environment name
 CONDA_ENV=bgcval2
 
-# Change this to your bgcval2 directory - or wheever you wish to run.
-#BGCVAL2_PATH=$PWD
-
-# Add one or more input_yml files here
-#BGCVAL2_SUITE2=input_yml/my_bgcval2_suite2.yml
 
 ##########################
 # Source global definitions
@@ -39,21 +39,15 @@ echo conda activate $CONDA_ENV
 conda activate ${CONDA_ENV}
 
 ##########################
-# Change directory to your bgcval2 directory:
-#echo cd $BGCVAL2_PATH
-#cd $BGCVAL2_PATH
-#pwd
-
-##########################
 # Load command line arguments:
 args=$@
 jobID=$1
 suites="${@:2}"
 echo $suites
 
+##########################
+# Run single jog timeseries analysis.
 echo "analysis_timeseries -j $jobID -k $suites"
 analysis_timeseries -j ${jobID} -k ${suites}
-#########################
-# Add one or more input_yml files here
-#cho analysis_timeseries -y $BGCVAL2_SUITE
-#nalysis_timeseries -y $BGCVAL2_SUITE 
+
+
