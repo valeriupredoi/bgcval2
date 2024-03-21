79971aa8ddb89e7c59f9f5ca9dd65147cf685adf
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-03-21 15:36:37 +0000

    add supported python to setup

--- setup.py
+++ setup.py
@@ -176,6 +176,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Atmospheric Science',
         'Topic :: Scientific/Engineering :: GIS',
