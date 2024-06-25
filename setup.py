205b88827f4b4bbab1c98e9d62fc16777dea9621
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-06-25 14:40:28 +0100

    comment out basemap detect other incompatibilities

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ REQUIREMENTS = {
     # Installation dependencies
     # Use with pip install . to install from source
     'install': [
-        'basemap>=1.3.6',
+        # 'basemap>=1.3.6',
         'cartopy',
         'jsondiff',
         'matplotlib',
