f75282d3aa40de9231d568e28b5d27ccf86f2d1a
Lee de Mora <ledm@pml.ac.uk>
2025-10-03 14:37:01 +0100

    typo fix

--- bgcval2/bgcvaltools/pftnames.py
+++ bgcval2/bgcvaltools/pftnames.py
@@ -356,9 +356,9 @@ def makeLongNameDict():
     lnd['DavisStraightSaltFlux'] = 'Davis Straight Salt Flux'
     lnd['DavisStraightMassFlux'] = 'Davis Straight Salt Flux'
     lnd['DavisStraightHeatFlux'] = 'Davis Straight Salt Flux'
-    lnd['NorwegeanSeaSaltFlux'] = 'Norwegean Sea Salt Flux'    
-    lnd['NorwegeanSeaMassFlux'] = 'Norwegean Sea Mass Flux'    
-    lnd['NorwegeanSeaHeatFlux'] = 'Norwegean Sea Heat Flux'
+    lnd['NorwegeanSeaSaltFlux'] = 'Norwegian Sea Salt Flux'    
+    lnd['NorwegeanSeaMassFlux'] = 'Norwegian Sea Mass Flux'    
+    lnd['NorwegeanSeaHeatFlux'] = 'Norwegian Sea Heat Flux'
 
     lnd['t_mn'] = 'Mean Temperature'
     lnd['t_an'] = 'Temperature'
