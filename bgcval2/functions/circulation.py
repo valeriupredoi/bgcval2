b8675b4c9bbe206e6952393aa44b19f4620ca5e6
Lee de Mora <ledm@pml.ac.uk>
2026-06-09 10:54:23 +0100

    Added recent changes to files.

--- bgcval2/functions/circulation.py
+++ bgcval2/functions/circulation.py
@@ -861,7 +861,7 @@ def gulfstream(nc, keys, **kwargs):
         if not loadedAltMask:
              loadAtlanticMask(altmaskfile, maskname='tmaskatl', grid=grid)
 
-        if nc.variables[keys[0]].shape == (332, 362):
+        if nc.variables['nav_lat'].shape == (332, 362):
             lonslice_70W = slice(207, 220) 
             gs_e1v = e1v_AMOC26N
             print('Classic grid')
@@ -871,7 +871,8 @@ def gulfstream(nc, keys, **kwargs):
             gs_e1v = e1v_AMOC26N
 
         else:
-            print('grid not recognised:', nc.variables[keys[0]].shape, grid)
+            print('grid not recognised:', nc.variables[keys[0]].shape, grid, keys)
+            print('in ', nc.filename)
             assert 0
     elif grid == 'eORCA025':
         latslice26Nnm = eORCA025_latslice26Nnm
